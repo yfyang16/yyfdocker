98b67402539b58c9e0cb40e824af65572fee4a68
yangyf <yangyf@rc.inesa.com>
2020-12-11 22:17:23 +0000

    format modification

--- container/container_process.go
+++ container/container_process.go
@@ -18,6 +18,7 @@ func NewParentProcess(tty bool, command string) *exec.Cmd {
 
 	args := []string{"init", command}
 	cmd := exec.Command("/proc/self/exe", args...)
+	
     cmd.SysProcAttr = &syscall.SysProcAttr{
         Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS |
 		syscall.CLONE_NEWNET | syscall.CLONE_NEWIPC,
