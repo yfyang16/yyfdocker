db9b28e73a7855ff2da18e3a55ca827a61bcba9b
yangyf <yangyf@rc.inesa.com>
2020-12-11 22:34:37 +0000

    yyfdocker run log error

--- run.go
+++ run.go
@@ -17,7 +17,7 @@ func Run(tty bool, command string) {
 
     parent := container.NewParentProcess(tty, command)
     if err := parent.Start(); err != nil {
-        log.Error(err)
+        log.Printf(err)
     }
     parent.Wait()
 
